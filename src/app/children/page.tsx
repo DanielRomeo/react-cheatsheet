af92510437666be99c1f1a50b1dd1b54a498b9ac
danielromeo <danielromeo99@gmail.com>
2023-11-03 10:32:23 +0200

    added children, PropsWithChildren type

---
+++ src/app/children/page.tsx
@@ -0,0 +1,49 @@
+import {useState, PropsWithChildren} from 'react'
+
+type myPropsType = {
+    isOpen: boolean,
+    onClose: ()=> void 
+}
+
+function Modal({ isOpen, onClose, children }: PropsWithChildren<myPropsType>) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        {children}
+      </div>
+      <button onClick={onClose} className="modal-close-button">
+        Close
+      </button>
+    </div>
+  );
+}
+
+export default function Home(){
+  const showLoginFormModal = true;
+  const showRegistrationFormModal = false;
+
+  return (
+    <div>
+      <Modal isOpen={showLoginFormModal} onClose={() => console.log("Login modal closed")}>
+        <h2>Login</h2>
+        <form>
+          {/* Form fields for login */}
+          Login form fields
+        </form>
+      </Modal>
+
+      <Modal isOpen={showRegistrationFormModal} onClose={() => console.log("Registration modal closed")}>
+        <h2>Register</h2>
+        <form>
+          {/* Form fields for registration */}
+          Registration form fields
+        </form>
+      </Modal>
+    </div>
+  );
+}
+
--- src/app/context/page.tsx
+++ src/app/context/page.tsx
@@ -31,16 +31,22 @@ export const useThemeContext = () => {
 
  
 export const ThemeSwitch = () => {
-    const [theme, setTheme] = useThemeContext();
-   
-    return (
-      <div>
-        <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-          Toggle theme
-        </button>
-      </div>
-    );
-  };
+  const [theme, setTheme] = useThemeContext();
+  
+  return (
+    <div>
+      <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        Toggle theme
+      </button>
+    </div>
+  );
+};
+
+const CustomComponent : React.FC= ()=>{
+  return (<div>
+    
+  </div>)
+}
 
   export const Water = () => {
     const [theme, setTheme] = useThemeContext();
